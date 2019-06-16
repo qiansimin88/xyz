cf87d90fc3ebaea772adfb5313640269085d0295
qsm <348867341@qq.com>
2019-06-16 20:44:04 +0800

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="test">
-    更新122223333
+    更新122223333222256
     <h1>本场比赛得分： {{ handlerCount }}</h1>
     <button @click="addAction(3)">加分</button>
     <button @click="reduce">减分</button>
